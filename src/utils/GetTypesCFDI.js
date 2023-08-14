0cff3d77dab7f39fb74f7fc45d4a80a86a927861
FReptar0 <20213tn137@utez.edu.mx>
2023-08-14 10:50:55 -0600

    Request of types updated

--- src/utils/GetTypesCFDI.js
+++ src/utils/GetTypesCFDI.js
@@ -25,7 +25,7 @@ async function getTypeP(index) {
     let year = new Date().getFullYear();
 
     try {
-        const response = await axios.get(`${url}/api/1.0/extern/tenants/${tenantIds[index]}/cfdis?to=${year}-${month}-${date}&from=${year}-01-01&cfdiType=PAYMENT_CFDI`, {
+        const response = await axios.get(`${url}/api/1.0/extern/tenants/${tenantIds[index]}/cfdis?createdUntil=${year}-${month}-${date}&createdFrom=${year}-01-01&documentTypes=CFDI&cfdiType=PAYMENT_CFDI`, {
             headers: {
                 'PDPTenantKey': apiKeys[index],
                 'PDPTenantSecret': apiSecrets[index]
@@ -98,7 +98,7 @@ async function getTypeI(index) {
     let year = new Date().getFullYear();
 
     try {
-        const response = await axios.get(`${url}/api/1.0/extern/tenants/${tenantIds[index]}/cfdis?to=${year}-${month}-${date}&from=${year}-07-01&cfdiType=INVOICE&stage=PENDING_TO_PAY`, {
+        const response = await axios.get(`${url}/api/1.0/extern/tenants/${tenantIds[index]}/cfdis?createdUntil=${year}-${month}-${date}&createdFrom=${year}-07-01&documentTypes=CFDI&cfdiType=INVOICE`, {
             headers: {
                 'PDPTenantKey': apiKeys[index],
                 'PDPTenantSecret': apiSecrets[index]
@@ -161,7 +161,7 @@ async function getTypeE(index) {
     let year = new Date().getFullYear();
 
     try {
-        const response = await axios.get(`${url}/api/1.0/extern/tenants/${tenantIds[index]}/cfdis?to=${year}-${month}-${date}&from=${year}-07-01&cfdiType=CREDIT_NOTE`, {
+        const response = await axios.get(`${url}/api/1.0/extern/tenants/${tenantIds[index]}/cfdis?createdUntil=${year}-${month}-${date}&createdFrom=${year}-07-01&documentTypes=CFDI&cfdiType=CREDIT_NOTE`, {
             headers: {
                 'PDPTenantKey': apiKeys[index],
                 'PDPTenantSecret': apiSecrets[index]
